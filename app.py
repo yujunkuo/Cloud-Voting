c2b5994a0f6bcb6eef310782bb032fc1fc526971
yujunkuo <johnboy880313@gmail.com>
2022-06-04 22:30:49 +0800

    init commit

---
+++ app.py
@@ -0,0 +1,60 @@
+import datetime
+from flask import Flask, render_template
+from google.cloud import bigtable
+from google.cloud.bigtable import column_family
+from google.cloud.bigtable import row_filters
+
+app = Flask(__name__)
+
+# [START bigtable_hw_connect]
+
+# The client must be created with admin=True because it will create a table
+client = bigtable.Client(project=project_id, admin=True)
+instance = client.instance(instance_id)
+table = instance.table(table_id)
+
+# [END bigtable_hw_connect]
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/vote/")
+def vote():
+    return render_template('vote.html')
+
+def write_one_vote_to_bigtable(account_id, candidate_id):
+    print("Writing some greetings to the table.")
+    greetings = ["Hello World!", "Hello Cloud Bigtable!", "Hello Python!"]
+    rows = []
+    column = "greeting".encode()
+    for i, value in enumerate(greetings):
+        row_key = "greeting{}".format(i).encode()
+        row = table.direct_row(row_key)
+        row.set_cell(
+            column_family_id, column, value, timestamp=datetime.datetime.utcnow()
+        )
+        rows.append(row)
+    table.mutate_rows(rows)
+
+def read_one_vote_from_bigtable(account_id):
+    # Create a filter to only retrieve the most recent version of the cell for each column accross entire row.
+    row_filter = row_filters.CellsColumnLimitFilter(1)
+    print("Getting a single greeting by row key.")
+    key = "greeting0".encode()
+    row = table.read_row(key, row_filter)
+    cell = row.cells[column_family_id][column][0]
+    print(cell.value.decode("utf-8"))
+
+def read_all_votes_from_bigtable():
+    # Create a filter to only retrieve the most recent version of the cell for each column accross entire row.
+    row_filter = row_filters.CellsColumnLimitFilter(1)
+    print("Scanning for all greetings:")
+    partial_rows = table.read_rows(filter_=row_filter)
+    for row in partial_rows:
+        cell = row.cells[column_family_id][column][0]
+        print(cell.value.decode("utf-8"))
+
+
+if __name__ == '__main__':
+    app.run()
